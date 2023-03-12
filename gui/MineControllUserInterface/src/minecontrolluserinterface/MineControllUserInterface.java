5b5267e14827078ece7be09fb329ee3af41161bf
Dusan Stijovic <dusan.stijovic598@gmail.com>
2023-03-12 12:37:05 +0100

    Dodat gui

---
+++ gui/MineControllUserInterface/src/minecontrolluserinterface/MineControllUserInterface.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minecontrolluserinterface;
+
+/**
+ *
+ * @author sd213335m
+ */
+public class MineControllUserInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
